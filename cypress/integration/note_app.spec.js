c809277b761d9a540843ed11e88cbdbba6f67c7f
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-09-23 11:46:51 -0300

    fix: optimize for testing

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,6 +1,15 @@
 describe('Note App', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      name: 'tester',
+      username: 'test',
+      password: 'test'
+    }
+
+    cy.request('POST', 'http://localhost:3001/api/users', user)
   })
 
   it('frontpage can be opened', () => {
@@ -13,15 +22,24 @@ describe('Note App', () => {
 
   it('user can login', () => {
     cy.contains('Show login').click()
-    cy.get('[name="Username"]').type('lucas')
-    cy.get('[name="Password"]').type('lucas')
+    cy.get('[name="Username"]').type('test')
+    cy.get('[name="Password"]').type('test')
     cy.contains('Login').click()
+    cy.contains('Add new note')
+  })
+
+  it('login fails with wrong password', () => {
+    cy.contains('Show login').click()
+    cy.get('[name="Username"]').type('badreq')
+    cy.get('[name="Password"]').type('badreq')
+    cy.contains('Login').click()
+    cy.contains('Wrong username or password')
   })
 
   it('user can add notes', () => {
     cy.contains('Show login').click()
-    cy.get('[name="Username"]').type('lucas')
-    cy.get('[name="Password"]').type('lucas')
+    cy.get('[name="Username"]').type('test')
+    cy.get('[name="Password"]').type('test')
     cy.contains('Login').click()
     cy.contains('Add new note').click()
     cy.get('input:first').type('Nota de prueba con cypress')
